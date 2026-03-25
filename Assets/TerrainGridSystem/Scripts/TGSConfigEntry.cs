2fc50036979694facff47ea79474e8a0b1150c0f
SeanSong <ssbxiaoge@163.com>
2026-03-25 20:43:13 +0800

    update1.0.8

---
+++ Assets/TerrainGridSystem/Scripts/TGSConfigEntry.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System;
+
+namespace TGS {
+
+    [Serializable]
+    public struct TGSConfigEntry {
+        public bool visible;
+        public bool visibleAlways;
+        public int territoryIndex;
+        public Color color;
+        public int textureIndex;
+        public int tag;
+        public bool canCross;
+        public float crossCost; // all sides
+        public float[] crossSidesCost; // per side
+        public Vector2 textureScale, textureOffset;
+        public TGSConfigAttribPair[] attribData;
+    }
+
+
+    [Serializable]
+    public struct TGSConfigAttribPair {
+        public string key;
+        public string stringValue;
+        public float numericValue;
+    }
+
+}
